30106cc1a498cd6d86d6f66bc0a9d6c2e672e561
RedaMG1 <mreda.elalaoui1@gmail.com>
2023-12-16 22:00:18 +0100

    travel 05

---
+++ src/Controller/DetailController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TourRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DetailController extends AbstractController
+{
+    #[Route('/detail', name: 'app_detail')]
+    public function index(): Response
+    {
+        return $this->render('detail/index.html.twig', [
+            'controller_name' => 'DetailController',
+        ]);
+    }
+
+        #[Route('/detail/{id}', name: 'detail_display')]
+        public function display(TourRepository $tourRepository,$id): Response
+        {
+            $tours = $tourRepository->findBy(['id'=>$id]);
+
+            return $this->render('detail/index.html.twig', [
+                'tours' => $tours,
+            ]);
+        }
+}
